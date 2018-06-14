20dcf9a41a30ec2b705e37d5332f4972f750d05e
ruslan.lopez <ruslan.lopez@globant.com>
2018-06-14 00:14:42 -0500

    addded flag to fix compilation
    (cherry picked from commit 1d3819f1514468e9e3d7d714fd2e9444c6846593)

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,27 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const ts = require('gulp-typescript');
-const sass = require('gulp-sass');
 const tsProject = ts.createProject('tsconfig.json', {
     outFile: 'kimera.js'
 });
 
 /**Default task (runs when executing `gulp` comand in the console)*/
 gulp.task('default', () => {
-    gulp.watch('./ts/*.ts', ['compile:ts', 'minify']);
-});
-
-// the task for sass compiler
-
-gulp.task('sass', () => {
-    gulp.src('./scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+    gulp.watch('ts/*.ts', ['compile:ts', 'minify']);
 });
 
 /**Task that compiles typescript code.*/
 gulp.task('compile:ts', function() {
-    var tsResult = gulp.src("./ts/*.ts")
+    var tsResult = gulp.src("ts/*.ts")
         .pipe(tsProject());
 
-    return tsResult.js.pipe(gulp.dest('./js/'));
+    return tsResult.js.pipe(gulp.dest('js/'));
 });
 
 
 /**Minifies kimera.js*/
 gulp.task('minify', () => {
-    return gulp.src('./js/kimera.js')
+    return gulp.src('js/kimera.js')
         .pipe(uglify({ compress: { drop_console: true }, mangle: false }).on('error', function(e) {
             console.log('Error uglify: ' + e);
         }))
